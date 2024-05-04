3938ac027b92e60c3e43dd0a14c713071c4c7e87
NinaZurash <nino.zurashvili.nz@gmail.com>
2024-05-04 14:39:11 +0400

    implement products listing

---
+++ src/services/useFetchModels.ts
@@ -0,0 +1,22 @@
+"use client";
+
+import { useMutation } from "@tanstack/react-query";
+import { useToast } from "../components/ui/use-toast";
+import { SITE_ROUTES } from "@/routes/site_routes";
+
+export function useFetchModels() {
+  const { toast } = useToast();
+  return useMutation({
+    mutationFn: async () => {
+      const response = await fetch(`${SITE_ROUTES.base}/api/models`);
+      return response.json();
+    },
+    onError: () => {
+      toast({
+        variant: "destructive",
+        title: "Oops!",
+        description: "There was a problem with your request.",
+      });
+    },
+  });
+}
